338735a06c4c12812713a9bc615b5e730eccf3f7
AndyGFX <o.kollar@gmail.com>
2017-10-22 20:39:21 +0200

    Prepare project

---
+++ _assets_/TestLevel_0/TestTileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TiledTileTemplate" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="TiledTileTemplate.png" trans="ff00ff" width="128" height="128"/>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
