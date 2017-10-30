670452ccf58e5563d1c8ab87a3d64b4430009d2e
AndyGFX <o.kollar@gmail.com>
2017-10-30 20:23:40 +0100

    NEW: Extract entities item defintion to extra script

--- Entities/POST_TiledLevelBuilder.gd
+++ Entities/POST_TiledLevelBuilder.gd
@@ -1,63 +1,6 @@
-extends Node
-
-# ---------------------------------------------------------------------
-# entity type as list - item is defined via item_id
-# ---------------------------------------------------------------------
-
-var ent_coin = [
-	load("res://Entities/Coin/Entity_Coin_0.tscn"),
-	load("res://Entities/Coin/Entity_Coin_1.tscn"),
-	load("res://Entities/Coin/Entity_Coin_2.tscn"),
-	load("res://Entities/Coin/Entity_Coin_3.tscn")]
-
-var ent_key = [
-	load("res://Entities/Key/Entity_Key_0.tscn"),
-	load("res://Entities/Key/Entity_Key_1.tscn"),
-	load("res://Entities/Key/Entity_Key_2.tscn"),
-	load("res://Entities/Key/Entity_Key_3.tscn")]
-
-var ent_ammo = [
-	load("res://Entities/Ammo/Entity_Ammo_0.tscn"),
-	load("res://Entities/Ammo/Entity_Ammo_1.tscn"),
-	load("res://Entities/Ammo/Entity_Ammo_2.tscn"),
-	load("res://Entities/Ammo/Entity_Ammo_3.tscn")]
-
-var ent_health = [
-	load("res://Entities/Health/Entity_Health_0.tscn"),
-	load("res://Entities/Health/Entity_Health_1.tscn"),
-	load("res://Entities/Health/Entity_Health_2.tscn"),
-	load("res://Entities/Health/Entity_Health_3.tscn")]
+extends "res://Entities/POST_EntitiesItemsDefintion.gd"
 
 
-var ent_gravity = [load("res://Entities/PowerUp_Gravity/Entity_PowerUpGravity_0.tscn")]
-
-var ent_jump = [load("res://Entities/PowerUp_Jump/Entity_PowerUpJump_0.tscn")]
-
-var ent_speed = [load("res://Entities/PowerUp_Speed/Entity_PowerUpSpeed_0.tscn")]
-
-var ent_infomsg = [load("res://Entities/InfoMsg/Entity_InfoMsg_0.tscn")]
-
-var ent_startpoint = [load("res://Entities/StartPoint/Entity_StartPoint_0.tscn")]
-
-var ent_endpoint = [load("res://Entities/EndPoint/Entity_EndPoint_0.tscn")]
-
-
-var ent_teleport = [
-	load("res://Entities/Teleport/Entity_Teleport_0.tscn"),
-	load("res://Entities/Teleport/Entity_Teleport_1.tscn"),
-	load("res://Entities/Teleport/Entity_Teleport_2.tscn"),
-	load("res://Entities/Teleport/Entity_Teleport_3.tscn")]
-
-var ent_enemy_h = [
-	load("res://Entities/Enemies/Enemy_H/Entity_Enemy_0.tscn"),
-	load("res://Entities/Enemies/Enemy_H/Entity_Enemy_1.tscn")]
-
-var ent_switch = [
-	load("res://Entities/Switch/Entity_Switch_0.tscn")]
-	
-var ent_light = [
-	load("res://Entities/Light/Entity_Light2D_0.tscn")]	
-	
 # ---------------------------------------------------------------------
 # Traverse the node tree and replace Tiled objects and Nodes
 # ---------------------------------------------------------------------
